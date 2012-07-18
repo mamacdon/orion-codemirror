6f3df6c457a70845f0ddea5a5a7d46215e53ad1b
Mark Macdonald <mamacdon@ca.ibm.com>
2012-07-18 14:43:06 -0400

    Shell script inanity

--- build/shove.sh
+++ build/shove.sh
@@ -1,34 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 projDir=~/workspace/dev/foo/cr
 buildDir=$projDir/build
 outDir=$projDir/out
 
-cd $projDir
+function check_err() {
+	if [[ "$1" -ne "0" ]]; then
+		echo $2
+		exit $1
+	fi
+}
 
-# merge in latest from master
-echo git checkout gh-pages
-echo git merge master
+# reset gh-pages to latest in master
+cd $projDir
+echo "Checking out gh-pages..."
+git checkout -f gh-pages
+check_err $? "checkout failed."
+echo "Resetting gh-pages to master..."
+git reset --hard master
+check_err $? "reset failed."
 
 # run the build
 cd $buildDir
 ./build.sh
-
-buildresult=$?
-if [[ ! $buildresult -eq 0 ]]; then
-	echo Build failed.
-	exit 1
-fi
+check_err $? "Build failed."
 
 # copy the resulting codeMirrorPlugin.html and lib/ to the toplevel project folder, so http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html will work
-cd $outdir
-cp codeMirrorPlugin.html ../
-cp lib ../
+cp $outDir/codeMirrorPlugin.html $projDir
+cp -r $outDir/lib $projDir
 
 # Commit the change from previous step
-git add ../codeMirrorPlugin.html
-git add ../lib
+git add $outDir/codeMirrorPlugin.html
+git add $outDir/lib
 git commit -m "Copy built code to project root for gh-pages"
 
+# clean up outDir
+git clean -df $outDir
+
+git co master
+echo 
 echo To update the GitHub page, run:
+echo 
 echo git push origin gh-pages
-# echo git co master
+
