f002c2ca6da3ccf62beba8d0a77d872c6f9fccba
Mark Macdonald <mamacdon@ca.ibm.com>
2012-07-18 13:05:10 -0400

    Add r.js optimizer, update build script so it runs r.js

---
+++ app.build.js
@@ -0,0 +1,12 @@
+({
+	appDir: '.',
+	baseUrl: 'lib',
+	dir: 'out/',
+	modules: [
+		{	name: "codeMirrorPlugin"
+		}
+	],
+	paths: {
+		i18n: 'requirejs/i18n'
+	}
+})
--- build.sh
+++ build.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
-
-EXPECTED_ARGS=1
-if [ $# -ne $EXPECTED_ARGS ]
-then
-    echo "Usage: build [output directory]"
-    exit 1
+outdir=$(pwd)/out
+if [[ -d "$outdir" ]]; then
+    echo Cleaning "$outdir"...
+    rm -rf $outdir
 fi
 
-mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-out=$1
-out="$( cd "${out}" && pwd )"
-
-if [ ! -d "$mydir" ]
-then
-    echo "build: couldn't find script working directory '${mydir}'"
-    exit 3
-fi
-
-if [ "$out" = "$mydir" ]
-then
-    echo "build: source and target directories are the same."
-    exit 4
-fi
-
-if [ ! -d "$out" ]
-then
-    mkdir ${out}
-fi
-
-cp -fR ${mydir}/* ${out}
-rm ${out}/lib/codemirror2
-echo "Built to ${out}."
-
+echo "Running build, output folder: $outdir"
+node r.js -o app.build.js
