d96e19765350311f3a157d9d2ad17c0e67b9b126
Mark Macdonald <mamacdon@ca.ibm.com>
2012-07-18 15:25:31 -0400

    Better naming and pushing

--- build/deploy.sh
+++ build/deploy.sh
@@ -24,21 +24,26 @@ cd $buildDir
 ./build.sh
 check_err $? "Build failed."
 
-# copy the resulting codeMirrorPlugin.html and lib/ to the toplevel project folder, so http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html will work
+# Commit the built code
+git add $outDir
+git commit -m "Update built code for gh-pages"
+
+# Copy the resulting codeMirrorPlugin.html and lib/ to the toplevel project folder, so http://mamacdon.github.com/orion-codemirror/codeMirrorPlugin.html will work
 cp $outDir/codeMirrorPlugin.html $projDir
 cp -r $outDir/lib $projDir
 
 # Commit the change from previous step
-git add $outDir/codeMirrorPlugin.html
-git add $outDir/lib
+git add $projDir/codeMirrorPlugin.html
+git add $projDir/lib
 git commit -m "Copy built code to project root for gh-pages"
 
-# clean up outDir
-git clean -df $outDir
-
+# clean up
 git co master
+git clean -df $projDir/codeMirrorPlugin.html
+git clean -df $projDir/lib
+git clean -df $projDir/out
+
 echo 
 echo To update the GitHub page, run:
 echo 
-echo git push origin gh-pages
-
+echo git push -f origin gh-pages:refs/heads/gh-pages
