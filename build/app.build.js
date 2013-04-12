803df87f1f3f58b9e2aef87535a80b4e59b49ba8
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-04-12 12:46:25 -0400

    Fix build file for shim and new module prefixes

--- build/app.build.js
+++ build/app.build.js
@@ -7,6 +7,14 @@
 		}
 	],
 	paths: {
+		cm2: 'codemirror2-compressed',
+		'orion-cm': 'orioncodemirror',
 		i18n: 'requirejs/i18n'
+	},
+	shim: {
+		// Before loading the modes this we need to setup the CodeMirror global
+		'cm2/modes-compressed': {
+			deps: ['orion-cm/globalCodeMirror']
+		}
 	}
 })
