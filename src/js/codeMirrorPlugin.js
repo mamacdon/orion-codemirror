73e7873a67e9ae5a075097ee0373c56fb90ffb77
Mark Macdonald <mamacdon@gmail.com>
2013-10-08 12:45:43 -0400

    Bump plugin version to 4.0

--- src/js/codeMirrorPlugin.js
+++ src/js/codeMirrorPlugin.js
@@ -133,7 +133,7 @@ require(['orion/plugin', 'orion/EventTarget', 'orion/textview/textModel',
 
 				var provider = new PluginProvider({
 					name: "Orion CodeMirror syntax highlighting",
-					version: "2.0",
+					version: "4.0",
 					description: "Provides syntax highlighting for various languages. Powered by code from the CodeMirror project.",
 					license: "Eclipse Distribution License",
 					website: "https://github.com/mamacdon/orion-codemirror"
